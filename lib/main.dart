df0bcd78d8f9381294808d4258c3bdb3b486ba15
Sandun C Cooray <86885937+SandunC000@users.noreply.github.com>
2025-04-28 21:30:00 +0530

    dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Guitar Codes',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
+      theme: lightThemeData(),
+      darkTheme: darkThemeData(),
+      themeMode: ThemeMode.system,
       home: const SplashScreen(),
+    );
+  }
+
+  ThemeData lightThemeData() {
+    return ThemeData(
+      scaffoldBackgroundColor: const Color(0xFFFBF5F3),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF43C59E),
+        brightness: Brightness.light,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFF646E78),
+        foregroundColor: Colors.white,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Color(0xFF0F0E0E)),
+        bodyMedium: TextStyle(color: Color(0xFF646E78)),
+      ),
+      useMaterial3: true,
+    );
+  }
+
+  ThemeData darkThemeData() {
+    return ThemeData(
+      scaffoldBackgroundColor: const Color(0xFF0F0E0E),
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: const Color(0xFF43C59E),
+        brightness: Brightness.dark,
+      ),
+      appBarTheme: const AppBarTheme(
+        backgroundColor: Color(0xFF646E78),
+        foregroundColor: Colors.white,
+      ),
+      textTheme: const TextTheme(
+        bodyLarge: TextStyle(color: Color(0xFFFBF5F3)), // text becomes light
+        bodyMedium: TextStyle(color: Color(0xFFCCCCCC)),
+      ),
+      useMaterial3: true,
     );
   }
 }
